536570999e1e5a0a18c546616006a045e7322caa
edumoisessilva <edumoisessilva@gmail.com>
2025-10-25 23:46:46 -0300

    alterando operador de comparação

--- n1/desafio_1.js
+++ n1/desafio_1.js
@@ -1,7 +1,7 @@
 function palindrome(palavra) {
   let limpar = palavra.toLowerCase().replace(/[^a-z0-9]/g, '');
   const palavraContrario = limpar.split('').reverse().join('');
-  return limpar == palavraContrario;
+  return limpar === palavraContrario;
 }
 
 palindrome("eye");
